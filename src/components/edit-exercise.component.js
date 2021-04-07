c2e9e64c7c17e490f1f4c20bf3d3f3ed416a284d
Jude Asiamah <judeasiamah@gmail.com>
2021-04-07 10:44:50 +0100

    Exercise React Component

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditExercise extends Component{
+    render(){
+        return(
+            <div className="container">
+                <p>You are on the Edit Exercise  Component</p>
+            </div>
+        );
+    }
+}
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ExerciseList extends Component{
+    render(){
+        return(
+            <div className="container">
+                <p>You are on the Exercise List Component</p>
+            </div>
+        );
+    }
+}
